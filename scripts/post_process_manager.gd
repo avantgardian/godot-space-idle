214f6b779c7ca56f8aa0f91a3e9d8256fac4e7fc
Andrei <andrew.avantgardian@gmail.com>
2026-07-19 11:00:46 +0100

    Add bloom post-process pass (bright-pass + 13-tap blur + additive composite) (#98)
    Closes #90

--- scripts/post_process_manager.gd
+++ scripts/post_process_manager.gd
@@ -3,6 +3,7 @@ extends Node
 
 var _mat: ShaderMaterial
 var _ca_impact: float = 0.0
+var _bloom_intensity: float = 0.7
 
 func _ready():
 	var pp_layer := CanvasLayer.new()
@@ -23,10 +24,19 @@ func _ready():
 	_mat = ShaderMaterial.new()
 	_mat.shader = preload("res://shaders/post_process.gdshader")
 	cr.material = _mat
+	_mat.set_shader_parameter("u_bloom_intensity", _bloom_intensity)
 
 func trigger():
 	_ca_impact = min(_ca_impact + 0.008, 0.015)
 	get_parent().get_node("Camera2D").trigger_shake(12.5)
+
+func set_bloom_intensity(value: float) -> void:
+	_bloom_intensity = clamp(value, 0.0, 2.0)
+	if _mat:
+		_mat.set_shader_parameter("u_bloom_intensity", _bloom_intensity)
+
+func get_bloom_intensity() -> float:
+	return _bloom_intensity
 
 func _process(delta):
 	if _ca_impact > 0.0:
